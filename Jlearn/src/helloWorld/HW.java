c6dfd443d961fee38254c16683245001ee608fca
EkremPC <ekremcek19@hotmail.com>
2018-03-04 22:42:09 +0300

    calc

--- Jlearn/src/helloWorld/HW.java
+++ Jlearn/src/helloWorld/HW.java
@@ -3,8 +3,15 @@ package helloWorld;
 public class HW {
 
     public static void main (String args[]){
-        System.out.println("hello home");
-        System.out.println("hello home2");
+        /*
+        first
+        //comm
+        second
+         */
+        //this is a standalone comment
+        System.out.println("hello home"); //comment 1
+        //System.out.println("hello home2");
+        System.out.println(/*"hello home3"*/"no no no");
 
     }
 }
