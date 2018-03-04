2e30ca115afa518d9c36dec055dcef90a2a6536d
EkremPC <ekremcek19@hotmail.com>
2018-03-04 23:34:44 +0300

    loops

--- homeLearning/Jlearn/src/helloWorld/HW.java
+++ homeLearning/Jlearn/src/helloWorld/HW.java
@@ -3,13 +3,27 @@ package helloWorld;
 public class HW {
 
     public static void main (String args[]){
+        int factorial = 100;
+        int key=4;
         /*
+        while (key>1)
+            factorial *= key--;
+
         first
         //comm
         second
          */
         //this is a standalone comment
-        System.out.println("hello home"); //comment 1
+        do {
+            System.out.print(key);
+            System.out.print(" * 2 = ");
+            System.out.println(key *= 2);
+        }
+        while(key < factorial);
+
+        for (int i = 4; i < 200; i *=2)
+            System.out.println(i);
+        System.out.println(factorial); //comment 1
         //System.out.println("hello home2");
         System.out.println(/*"hello home3"*/"no no no");
 
