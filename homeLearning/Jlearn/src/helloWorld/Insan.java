50c76e75c79296fd0ee1096149f097461d709089
EkremPC <ekremcek19@hotmail.com>
2018-03-24 13:43:21 +0300

    tests

---
+++ homeLearning/Jlearn/src/helloWorld/Insan.java
@@ -0,0 +1,41 @@
+package helloWorld;
+
+public class Insan {
+    public String getIsim() {
+        return isim;
+    }
+
+    public void setIsim(String isim) {
+        this.isim = isim;
+    }
+    private static String isim;
+
+    public String getSoyisim() {
+        return soyisim;
+    }
+
+    public void setSoyisim(String soyisim) {
+        this.soyisim = soyisim;
+    }
+
+    private String soyisim;
+    public int age;
+    int luckynumber;
+
+    public Insan(String insanismi) {
+        this.isim = insanismi;
+        adinisoyle();
+    }
+
+    public Insan(){
+
+
+    }
+
+    private static void adinisoyle(){
+        System.out.println("benim adim"+isim);
+    }
+
+    public void selamver(){}
+
+}
---
+++ homeLearning/Jlearn/src/helloWorld/Main.java
@@ -0,0 +1,17 @@
+package helloWorld;
+
+public class Main {
+
+    public static void main(String args[]){
+
+        Insan bahar = new Insan("Bahar");
+
+        Insan ekrem = new Insan();
+        ekrem.setIsim("Ekrem");
+        ekrem.age = 29;
+        ekrem.selamver();
+
+
+
+    }
+}
